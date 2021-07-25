be59ae8eded5ddca6240c5b585a5cef6eded1afb
Carlos <thethirdcage@gmail.com>
2021-07-25 19:18:07 +1000

    f

---
+++ universe/src/components/BasicScene.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+function BasicScene() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BasicScene
---
+++ universe/src/components/systems/firstSystem.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+function firstSystem(props) {
+    const { systemName } = props
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default firstSystem
+
+firstSystem.propTypes = {
+    systemName: PropTypes.string,
+}
